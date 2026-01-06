b5c6abc934b0c7b7f651164cf98c0cc319c22893
Vladimir <lismiroslav@yandex.ru>
2026-01-07 01:04:57 +0300

    статус бар на iPhone

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,33 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa'
+import {VitePWA} from 'vite-plugin-pwa'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
 
-  plugins: [vue(), tailwindcss(), VitePWA()],
-  resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: path.resolve(__dirname, "src"),
-      },
-    ]},
+    plugins: [vue(), tailwindcss(), VitePWA({
+            registerType: 'autoUpdate',
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+            },
+            manifest: {
+                name: 'project_m',
+                short_name: 'm',
+                start_url: '/',
+                display: 'standalone',
+                background_color: '#1e2939',
+                theme_color: '#1e2939'
+            }
+        }
+    )],
+    resolve: {
+        alias: [
+            {
+                find: "@",
+                replacement: path.resolve(__dirname, "src"),
+            },
+        ]
+    },
 })
