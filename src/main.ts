72400c244fee03d68d7da0b7535caa738f62a492
Vladimir <lismiroslav@yandex.ru>
2026-01-06 21:16:24 +0300

    подключил pinia, elementPlus, vue router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
 import './style.css'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.use(ElementPlus)
+app.mount('#app')
