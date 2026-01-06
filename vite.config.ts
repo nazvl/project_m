6901510529ecd1359a025748b4c225ee5da7b608
Vladimir <lismiroslav@yandex.ru>
2026-01-07 00:39:21 +0300

    vite plugin pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
 
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), VitePWA()],
   resolve: {
     alias: [
       {
